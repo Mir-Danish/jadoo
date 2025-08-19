1a58859b89befdbf47df24129d2276e8424ac321
mirdanish8767@gmail.com <mirdilber43@gmail.com>
2025-08-19 23:41:05 +0530

    added code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -30,18 +30,18 @@ const Home = () => {
                 </div>
 
                 <div className='category-service-box2'>
-                   <p className='category-service-box-text'>Weather Forecast</p>
-                   <p className='category-service-box-description'>Built Wicket longer admire do barton vanity itself do in it.</p>
+                   <p className='category-service-box-text'>Best Flights</p>
+                   <p className='category-service-box-description'>Engrossed listening. Park gate sell they west hard for the.</p>
                 </div>
 
                  <div className='category-service-box3'>
-                   <p className='category-service-box-text'>Weather Forecast</p>
-                   <p className='category-service-box-description'>Built Wicket longer admire do barton vanity itself do in it.</p>
+                   <p className='category-service-box-text'>Local Events</p>
+                   <p className='category-service-box-description'>Barton vanity itself do in it. Preferd to men it engrossed listening.</p>
                 </div>
 
                 <div className='category-service-box4'>
-                   <p className='category-service-box-text'>Weather Forecast</p>
-                   <p className='category-service-box-description'>Built Wicket longer admire do barton vanity itself do in it.</p>
+                   <p className='category-service-box-text'>Customization</p>
+                   <p className='category-service-box-description'>We deliver outsourcedaviation services for military customers</p>
                 </div>
                 
             </div>
